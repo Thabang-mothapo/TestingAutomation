2466dba3816e64183f235a7cd7fb632cfe96cb99
Thabang-mothapo <thabang.mothapo@sambeconsulting.com>
2025-06-20 13:36:00 +0200

    Update deploy_timesheet.sql

--- deploy_timesheet.sql
+++ deploy_timesheet.sql
@@ -2,6 +2,11 @@
 USE master;
 GO
 
+-- Drop the procedure if it exists to avoid Msg 2714
+IF OBJECT_ID('DeployTimesheetDatabase', 'P') IS NOT NULL
+    DROP PROCEDURE DeployTimesheetDatabase;
+GO
+
 -- Create the DeployTimesheetDatabase stored procedure
 CREATE PROCEDURE DeployTimesheetDatabase
 AS
@@ -15,23 +20,32 @@ BEGIN
             CREATE DATABASE Timesheet;
         END
 
-        -- Switch to Timesheet database (handled dynamically here)
-        -- Use dynamic SQL to avoid USE statement inside procedure
+        -- Use dynamic SQL to create tables in Timesheet database
         DECLARE @SQL NVARCHAR(MAX);
-        SET @SQL = N'USE Timesheet; ' +
-                  N'IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Consultant'') ' +
-                  N'CREATE TABLE Consultant (ConsultantId INT PRIMARY KEY, Name NVARCHAR(100), Email NVARCHAR(100)); ' +
-                  N'IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Client'') ' +
-                  N'CREATE TABLE Client (ClientId INT PRIMARY KEY, Name NVARCHAR(100), Contact NVARCHAR(100)); ' +
-                  N'IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Timesheet'') ' +
-                  N'CREATE TABLE Timesheet (TimesheetId INT PRIMARY KEY, ConsultantId INT, ClientId INT, DateWorked DATE, HoursWorked DECIMAL(5,2), FOREIGN KEY (ConsultantId) REFERENCES Consultant(ConsultantId), FOREIGN KEY (ClientId) REFERENCES Client(ClientId)); ' +
-                  N'IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Leave'') ' +
-                  N'CREATE TABLE Leave (LeaveId INT PRIMARY KEY, ConsultantId INT, StartDate DATE, EndDate DATE, Reason NVARCHAR(200), FOREIGN KEY (ConsultantId) REFERENCES Consultant(ConsultantId)); ' +
-                  N'IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''AuditLog'') ' +
-                  N'CREATE TABLE AuditLog (AuditLogId INT PRIMARY KEY IDENTITY, EventType NVARCHAR(100), EventDateTime DATETIME, Details NVARCHAR(500)); ' +
-                  N'IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''ErrorLog'') ' +
-                  N'CREATE TABLE ErrorLog (ErrorLogId INT PRIMARY KEY IDENTITY, ErrorMessage NVARCHAR(500), ErrorDateTime DATETIME, StackTrace NVARCHAR(MAX));';
+        SET @SQL = N'
+            USE Timesheet;
 
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Consultant'')
+            CREATE TABLE Consultant (ConsultantId INT PRIMARY KEY, Name NVARCHAR(100), Email NVARCHAR(100));
+
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Client'')
+            CREATE TABLE Client (ClientId INT PRIMARY KEY, Name NVARCHAR(100), Contact NVARCHAR(100));
+
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Timesheet'')
+            CREATE TABLE Timesheet (TimesheetId INT PRIMARY KEY, ConsultantId INT, ClientId INT, DateWorked DATE, HoursWorked DECIMAL(5,2),
+                                    FOREIGN KEY (ConsultantId) REFERENCES Consultant(ConsultantId),
+                                    FOREIGN KEY (ClientId) REFERENCES Client(ClientId));
+
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''Leave'')
+            CREATE TABLE Leave (LeaveId INT PRIMARY KEY, ConsultantId INT, StartDate DATE, EndDate DATE, Reason NVARCHAR(200),
+                                FOREIGN KEY (ConsultantId) REFERENCES Consultant(ConsultantId));
+
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''AuditLog'')
+            CREATE TABLE AuditLog (AuditLogId INT PRIMARY KEY IDENTITY, EventType NVARCHAR(100), EventDateTime DATETIME, Details NVARCHAR(500));
+
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''ErrorLog'')
+            CREATE TABLE ErrorLog (ErrorLogId INT PRIMARY KEY IDENTITY, ErrorMessage NVARCHAR(500), ErrorDateTime DATETIME, StackTrace NVARCHAR(MAX));
+        ';
         EXEC sp_executesql @SQL;
 
         -- Log successful deployment
